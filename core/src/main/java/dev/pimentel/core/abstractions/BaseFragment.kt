1b97d31785ff9a630e54018fa3aa0edb8302c3ff
Bruno Pimentel <bruno@pimentel.dev>
2020-03-31 11:58:17 -0300

    feat: create view binding abstraction for fragments

--- core/src/main/java/dev/pimentel/core/abstractions/BaseFragment.kt
+++ core/src/main/java/dev/pimentel/core/abstractions/BaseFragment.kt
@@ -1,27 +1,42 @@
 package dev.pimentel.core.abstractions
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import androidx.viewbinding.ViewBinding
 import org.koin.core.context.loadKoinModules
 import org.koin.core.context.unloadKoinModules
 import org.koin.core.module.Module
 
-abstract class BaseFragment<ViewModelType : BaseContract.ViewModel>(
+abstract class BaseFragment<ViewModelType, BindingType>(
     @LayoutRes layoutResId: Int
-) : Fragment(layoutResId) {
+) : Fragment(layoutResId), ViewBindingHolder<BindingType> by ViewBindingHolderImpl()
+        where ViewModelType : BaseContract.ViewModel,
+              BindingType : ViewBinding {
 
     abstract val modules: List<Module>
     abstract val viewModel: ViewModelType
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         loadKoinModules(modules)
-        bindViewModel()
+        return bindView()
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unloadKoinModules(modules)
+    }
+
+    abstract fun bindView(): View
 
     open fun bindViewModel() {
         viewModel.isLoading().observe {
@@ -35,11 +50,6 @@ abstract class BaseFragment<ViewModelType : BaseContract.ViewModel>(
         viewModel.error().observe {
             // TODO
         }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        unloadKoinModules(modules)
     }
 
     protected inline fun <ObserverType> LiveData<ObserverType>.observe(
--- feed/src/main/java/dev/pimentel/feed/presentation/FeedFragment.kt
+++ feed/src/main/java/dev/pimentel/feed/presentation/FeedFragment.kt
@@ -2,12 +2,26 @@ package dev.pimentel.feed.presentation
 
 import dev.pimentel.core.abstractions.BaseFragment
 import dev.pimentel.feed.R
+import dev.pimentel.feed.databinding.FeedFragmentLayoutBinding
 import dev.pimentel.feed.feedModules
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.core.module.Module
 
-class FeedFragment : BaseFragment<FeedContract.ViewModel>(R.layout.feed_fragment_layout) {
+class FeedFragment : BaseFragment<FeedContract.ViewModel, FeedFragmentLayoutBinding>(
+    R.layout.feed_fragment_layout
+) {
 
     override val modules: List<Module> = feedModules
     override val viewModel: FeedContract.ViewModel by viewModel<FeedViewModel>()
+
+    override fun bindView() = initBinding(
+        FeedFragmentLayoutBinding.inflate(layoutInflater),
+        this
+    ) {
+        viewModel.testText().observe {
+            feedTvTest.text = it
+        }
+
+        viewModel.initialize()
+    }
 }

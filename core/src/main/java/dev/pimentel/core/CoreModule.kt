eb7b0e4a0177ac385c80451cae321d11c063f509
Bruno Pimentel <bruno@pimentel.dev>
2020-04-08 12:40:08 -0300

    feat: adds api url, key and request interceptor

--- core/src/main/java/dev/pimentel/core/CoreModule.kt
+++ core/src/main/java/dev/pimentel/core/CoreModule.kt
@@ -3,10 +3,47 @@ package dev.pimentel.core
 import dev.pimentel.core.schedulerprovider.SchedulerProvider
 import dev.pimentel.core.schedulerprovider.SchedulerProviderImpl
 import dev.pimentel.core.usecases.GetErrorMessage
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+private const val REQUEST_TIMEOUT = 60L
+
+private val networkModule = module {
+    single {
+        val apiUrl = androidContext().getString(R.string.news_api_url)
+        val apiKey = androidContext().getString(R.string.news_api_key)
+
+        OkHttpClient.Builder()
+            .readTimeout(REQUEST_TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(REQUEST_TIMEOUT, TimeUnit.SECONDS)
+            .connectTimeout(REQUEST_TIMEOUT, TimeUnit.SECONDS)
+            .addInterceptor(RequestInterceptor(apiKey))
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
+            .let { client ->
+                Retrofit.Builder()
+                    .baseUrl(apiUrl)
+                    .client(client)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .build()
+
+            }
+    }
+}
 
 private val useCasesModule = module {
-    single { GetErrorMessage() }
+    single { GetErrorMessage(androidContext()) }
 }
 
 private val schedulerProviderModule = module {
@@ -15,5 +52,24 @@ private val schedulerProviderModule = module {
 
 val coreModules = listOf(
     useCasesModule,
-    schedulerProviderModule
+    schedulerProviderModule,
+    networkModule
 )
+
+internal class RequestInterceptor(
+    private val apiKey: String
+) : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+
+        return originalRequest.newBuilder()
+            .addHeader(AUTHORIZATION_HEADER_NAME, apiKey)
+            .build()
+            .let(chain::proceed)
+    }
+
+    private companion object {
+        const val AUTHORIZATION_HEADER_NAME = "Authorization"
+    }
+}
--- core/src/main/java/dev/pimentel/core/usecases/GetErrorMessage.kt
+++ core/src/main/java/dev/pimentel/core/usecases/GetErrorMessage.kt
@@ -1,9 +1,17 @@
 package dev.pimentel.core.usecases
 
+import android.content.Context
 import dev.pimentel.core.abstractions.UseCase
+import java.io.IOException
 
-class GetErrorMessage : UseCase<GetErrorMessageParams, String> {
-    override fun invoke(params: GetErrorMessageParams) = ""
+class GetErrorMessage(
+    private val context: Context
+) : UseCase<GetErrorMessageParams, String> {
+
+    override fun invoke(params: GetErrorMessageParams) = when (params.throwable) {
+        is IOException -> "NoConnectionError"
+        else -> "AnyError"
+    }
 }
 
 data class GetErrorMessageParams(
